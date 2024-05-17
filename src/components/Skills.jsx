adba476c3b89d838acd5118c23958a12dee1dbb1
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-05-17 07:02:58 +0200

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -23,8 +23,8 @@ const Skills = () => {
                 border-b-4 border-pink-600'>Skills</p>
                 <p className='py-4'>// These are the technologies I've worked with</p>
             </div>
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 '>
-            <div className='shadow-md shadow-[#ff4800] hover:scale-110 duration-500 cursor-pointer'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 cursor-pointer'>
+            <div className='shadow-md shadow-[#ff4800] hover:scale-110 duration-500 '>
                 <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
                 <p className='my-4'>HTML</p>
                 </div>
