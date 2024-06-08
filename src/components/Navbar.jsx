7a7b9817599dcf6b8689d3d0ac34bf1ff58942b8
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-06-08 23:03:25 +0200

    Update icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
-      {/* Mobile menu */}
+      {/* Mobile-menu */}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
         {navItems.map((item, index) => (
           <li key={index} className='py-6 text-4xl'>
