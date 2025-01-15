11c240a1352a710875588b1587b85d8128d21484
epemba1 <145719061+epemba1@users.noreply.github.com>
2025-01-15 22:36:38 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         <p className='text-pink-600 '>Hi, my name is </p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Pemba Eric</h1>
         <h2 className='tect-4xl sm:text-7cl font-bold text-[#8892b0]'>I'm a front-End Developer.</h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm currently in the second year of my master's. I'm focused on building responsive full-stack web applications</p>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>I have recently graduated with my master's degree. I'm focused on building responsive full-stack web applications</p>
         <div>
           <button className='text-white group border-2  px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 rounded'>
             View Project
