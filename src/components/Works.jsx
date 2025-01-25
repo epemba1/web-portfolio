f7c983164974e92ccf9f1f5de994ae26bc2c2fd0
epemba1 <145719061+epemba1@users.noreply.github.com>
2025-01-25 21:30:33 +0100

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -89,7 +89,28 @@ const Works = () => {
             </div>
           </div>
 
+          <div style={{ backgroundImage: `url(${youtube})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
 
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                YouTube Clone
+              </span>
+              <div className='pt-8 text-center '>
+                <a href="https://epemba1.github.io/youtube-clone-tailwind/public/index.html" target="_blank" rel="noopener noreferrer">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/epemba1/web-portfolio" target="_blank" rel="noopener noreferrer">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           
 
         </div>
